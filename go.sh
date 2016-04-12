0542f78408013aaedfe83862e9b7d03b17ef332b
xiaoyege <598595476@qq.com>
2016-04-12 18:12:16 +0800

    新建脚本

---
+++ go.sh
@@ -0,0 +1,7 @@
+git config --global user.email "598595476@qq.com"
+git config --global user.name "xiaoyege"
+git config --global push.default simple
+git add .
+git commit -m $1
+git push
+start http://xiaoyege.github.io
